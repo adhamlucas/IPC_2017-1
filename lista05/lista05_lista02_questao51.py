93fe8d33424bb96c6377cf6bf5cd7c78fa65c360
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-04-19 18:16:42 -0400

    Retirando cabeçalho desnecessário

--- lista05/lista05_lista02_questao51.py
+++ lista05/lista05_lista02_questao51.py
@@ -1,19 +1,3 @@
-﻿#----------------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
-# Universidade do Estado do Amazonas - UEA
-# Prof. Jucimar Jr
-# Evandro Padilha Barroso Filho     1715310009
-# Gabriel Barroso da Silva Lima     1715310011
-# Frederico Victor Alfaia Rodrigues 1515200030
-# Felipe Eduardo Silva de Almeida   1715310031
-# Felipe Guerreiro De Mello         1315120052
-# Silas castro de Mendonça          1715310066
-# Escreva um algoritmo em Python que calcule 
-# o quociente da divisão de A por B (número inteiros
-# e positivos), ou seja, A div B, através de subtrações
-# sucessivas. Esses dois valores são passados pelo usuário
-# através do teclado. 
-
 #----------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
