fae4ee0a49fa86aca7be81ffb9bea7bea10e1f3a
adhamlucas <adhamlucas14@hotmail.com>
2017-04-30 01:43:17 -0400

    sorry

--- lista06/lista06_lista01_questao05.py
+++ lista06/lista06_lista01_questao05.py
@@ -1,23 +1,18 @@
 #Faça um Programa que leia 20 números inteiros e armazene-os num vetor. 
 #Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores. 
 
-count = 0
 limit = 20
 vet = []
 vet_pair = []
 vet_odd = []
 
-
-while count < limit:
-    number = int(input('Digite o número %d: ' % (count+1)))
+for element in range(limit):
+    number = int(input('Digite o número %d: ' % (element+1)))
     vet.append(number)
-    count += 1
-
-for element in vet:
-    if element % 2 == 0:
-        vet_pair.append(element)
+    if number % 2 == 0:
+        vet_pair.append(number)
     else:
-        vet_odd.append(element)
+        vet_odd.append(number)
 
 print(vet)
 print(vet_pair)
