e0928c35521e544db410c0a660e35982b4658213
adhamlucas <adhamlucas14@hotmail.com>
2017-04-30 01:13:39 -0400

    Update lista06_lista01_questao02.py

--- lista06/lista06_lista01_questao02.py
+++ lista06/lista06_lista01_questao02.py
@@ -1,7 +1,8 @@
 #Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
 vet = []
 
-for i in range (1,10):
+for i in range (0,10):
 
     numbers = float(input())
     vet.append(numbers)
