084f5677e8d5a89dd574ea7cd044f82d8e696398
adhamlucas <adhamlucas14@hotmail.com>
2017-04-30 01:46:21 -0400

    Update lista06_lista01_questao05.py

--- lista06/lista06_lista01_questao05.py
+++ lista06/lista06_lista01_questao05.py
@@ -1,5 +1,17 @@
-#Faça um Programa que leia 20 números inteiros e armazene-os num vetor. 
-#Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores. 
+#----------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Alexandre Marques Uchôa           1715310028
+# Carlos Eduardo T. de Oliveira     1715310030
+# Hugo Tadeu Silva Cardoso          1715310013
+# Judá Salazar Braga                1515200050
+# Kethelen Tamara Braba Barbosa     1525212002
+#
+# Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+# Armazene os números pares no vetor PAR e os números IMPARES no vetor impar.
+# Imprima os três vetores.
+#---------------------------------------------------------------------------
 
 limit = 20
 vet = []
