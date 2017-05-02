b696a6914eac3a6bcc5df818f614030bb56619ed
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-05-02 00:07:20 -0400

    Subindo questão do grupo 1

---
+++ lista06/lista06_lista03_questao01.py
@@ -0,0 +1,26 @@
+#---------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+#
+# Adham Lucas da Silva Oliveira           1715310059
+# Gabriel Barroso da Silva Lima           1715310011
+# Guilherme Silva de Oliveira             1715310034
+# Natália Cavalcante Xavier               1715310021
+# Tiago Ferreira Aranha	                  1715310047
+#
+# Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura. 
+#---------------------------------------------------------------------------
+
+n = int(input("digite a quantidade de números: "))
+lista = []
+
+for i in range (0, n):
+    
+    numero = int(input("digite um número: "))
+    lista.append(numero)
+    
+while n > 0:
+    
+    n -= 1
+    print (lista[n])
