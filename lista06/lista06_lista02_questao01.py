aca4f455e36baccfe022181a85ee98d1d5f9888b
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-05-01 21:01:35 -0400

    Add files via upload

---
+++ lista06/lista06_lista02_questao01.py
@@ -0,0 +1,15 @@
+#---------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+# Adham Lucas da Silva Oliveira           1715310059
+# Gabriel Barroso da Silva Lima           1715310011
+# Guilherme Silva de Oliveira             1715310034
+# Natália Cavalcante Xavier              1715310021
+# Tiago Ferreira Aranha	                  1715310047
+#Escreva um algoritmo em PORTUGOL que armazene em um vetor todos os números inteiros de 0 a 50.
+#Após isso, o algoritmo deve imprimir todos os valores armazenados
+##---------------------------------------------------------------------------#
+lista=list(range(0,51))
+print(lista)
