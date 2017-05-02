d944fce58b55df3900c4649eb15daeb8ed55e2f0
tiagoweb <tiagoweb@gmail.com>
2017-05-02 09:09:56 -0400

    update

--- lista06/lista06_lista02_questao12.py
+++ lista06/lista06_lista02_questao12.py
@@ -1,28 +1,27 @@
-#---------------------------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
+# ---------------------------------------------------------------------------
+# Introduï¿½ï¿½o a Programaï¿½ï¿½o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
 # Adham Lucas da Silva Oliveira           1715310059
 # Gabriel Barroso da Silva Lima           1715310011
 # Guilherme Silva de Oliveira             1715310034
-# Natália Cavalcante Xavier               1715310021
+# Natï¿½lia Cavalcante Xavier               1715310021
 # Tiago Ferreira Aranha	                  1715310047
 #
 # Escreva um algoritmo em Python o de cada
-# número. Após isso, o algoritmo deve
+# nï¿½mero. Apï¿½s isso, o algoritmo deve
 # imprimir todos os valores armazenados.
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
 tamanho = 10
 quadrado = []
 
-for i in range (0, tamanho):
-    
+for i in range(0, tamanho):
     numero = int(input("digite um valor: "))
-    quadrado.append(numero**2)
-    
-print ("quadrado dos valores apresentados:")
-for i in range (0, tamanho):
-    
-    print (quadrado[i])
+    quadrado.append(numero ** 2)
+
+print("quadrado dos valores apresentados:")
+
+for numero in quadrado:
+    print(numero)
