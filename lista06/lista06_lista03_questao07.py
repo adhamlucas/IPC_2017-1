37184377675aada4dfaf3531964aa902b80f4fee
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-01 21:36:56 -0400

    Update lista06_lista03_questao07.py

--- lista06/lista06_lista03_questao07.py
+++ lista06/lista06_lista03_questao07.py
@@ -1,1 +1,25 @@
+sequence = input().split()
+size = len(sequence)
+times = []
+used_numbers = []
+repetitions = 0
+c = 0
+while c < size:
+    if sequence[c] not in used_numbers:
+        for i in sequence[:]:
+            if float(sequence[c]) == float (i):
+                repetitions += 1
+        used_numbers += [sequence[c]]
+        times += [repetitions]
+
+    c += 1
+    repetitions = 0
+
+c = 0
+while c < len(times):
+    if times[c] > 1:
+        print(used_numbers[c],"ocorre",times[c],"Vezes")
+    else:
+        print(used_numbers[c],"ocorre",times[c],"Vez")
+    c += 1
 
