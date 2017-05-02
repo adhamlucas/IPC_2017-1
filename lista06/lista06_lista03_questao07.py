c1fc034bcd600b773edaf367e8bd6e2d08c680e4
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-01 21:41:22 -0400

    Update lista06_lista03_questao07.py

--- lista06/lista06_lista03_questao07.py
+++ lista06/lista06_lista03_questao07.py
@@ -7,7 +7,7 @@
 # Felipe Eduardo Silva de Almeida       1715310031
 # Nayara da Silva Cerdeira da Costa     1715310038
 # Joelson Pereira Lima 			        1715310060
-# Ian Gabriel Costa Machado              1215120276
+# Ian Gabriel Costa Machado             1215120276
 #
 #7.(MAT 88) Dada uma seqüência de n números reais, determinar os números que compõem a seqüência e o número de vezes
 #que cada um deles ocorre na mesma.
@@ -21,7 +21,7 @@
 # 0.0 ocorre 2 vezes
 # 1.5 ocorre 1 vez
 # 2.3 ocorre 1 vez
-
+#----------------------------------------------------------------------------------------------------------------------
 
 sequence = input().split()
 size = len(sequence)
@@ -29,6 +29,7 @@ times = []
 used_numbers = []
 repetitions = 0
 c = 0
+
 while c < size:
     if sequence[c] not in used_numbers:
         for i in sequence[:]:
