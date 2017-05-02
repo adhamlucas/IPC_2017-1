2071e60c69628ab23f72b94a48c6104db56eab81
natcavalcantex21 <nataliacxavier1@gmail.com>
2017-05-01 20:52:27 -0400

    Update lista06_lista01_questao01.py

--- lista06/lista06_lista01_questao01.py
+++ lista06/lista06_lista01_questao01.py
@@ -6,7 +6,7 @@
 # Adham Lucas da Silva Oliveira           1715310059
 # Gabriel Barroso da Silva Lima           1715310011
 # Guilherme Silva de Oliveira             1715310034
-# Natália Cavalcantre Xavier              1715310021
+# Natália Cavalcante Xavier              1715310021
 # Tiago Ferreira Aranha	                  1715310047
 #
 #Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
