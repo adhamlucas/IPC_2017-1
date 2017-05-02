797a1b3ff07f2042b96d3e5e533d75100bc3f6f1
KethelenTamara <kettamara@gmail.com>
2017-05-01 20:09:56 -0400

    lista06_lista02_questao05.py
    lista06_lista02_questao05.py

---
+++ lista06/lista06_lista02_questao05.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Alexandre Marques Uchôa           1715310028
+# Carlos Eduardo T. de Oliveira     1715310030
+# Hugo Thadeu Silva Cardoso         1715310013
+# Judá Salazar Braga                1515200050
+# Kethelen Tamara Braba Barbosa     1525212002
+# 
+# Escreva um algoritmo em PORTUGOL que armazene em um vetor todos os números
+# inteiros de 100 a 200. Após isso, o algoritmo deve imprimir todos os valores
+# armazenados.
+#------------------------------------------------------------------------------
+
+l=[]
+for i in range(200,99,-1):
+    l.append(i)
+print(l)
