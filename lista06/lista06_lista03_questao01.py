f8368117803e4ef6dce3ef1e808120503b658140
tiagoweb <tiagoweb@gmail.com>
2017-05-02 09:13:00 -0400

    update

--- lista06/lista06_lista03_questao01.py
+++ lista06/lista06_lista03_questao01.py
@@ -1,26 +1,25 @@
-#---------------------------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
+# -*- coding: utf8 -*-
+# ---------------------------------------------------------------------------
+# IntroduÃ§Ã£o a ProgramaÃ§Ã£o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 #
 # Adham Lucas da Silva Oliveira           1715310059
 # Gabriel Barroso da Silva Lima           1715310011
 # Guilherme Silva de Oliveira             1715310034
-# Natália Cavalcante Xavier               1715310021
+# NatÃ¡lia Cavalcante Xavier               1715310021
 # Tiago Ferreira Aranha	                  1715310047
 #
-# Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura. 
+# Dada uma seqÃ¼Ãªncia de n nÃºmeros, imprimi-la na ordem inversa Ã  da leitura. 
 #---------------------------------------------------------------------------
 
-n = int(input("digite a quantidade de números: "))
+n = int(input("digite a quantidade de nÃºmeros: "))
 lista = []
 
 for i in range (0, n):
     
-    numero = int(input("digite um número: "))
+    numero = int(input("digite um nÃºmero: "))
     lista.append(numero)
     
-while n > 0:
-    
-    n -= 1
-    print (lista[n])
+for numero in lista[::-1]:
+    print(numero)
