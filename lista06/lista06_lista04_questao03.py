2018acb9622366c5099950a21c2f3f959cd821fc
tiagoweb <tiagoweb@gmail.com>
2017-05-03 15:45:27 -0400

    update

--- lista06/lista06_lista04_questao03.py
+++ lista06/lista06_lista04_questao03.py
@@ -1,11 +1,12 @@
-#---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-# Lista 4. Q 3 - Nome na vertical. Faça um programa que solicite o nome
-#       do usuário e imprima-o na vertical.
+# Lista 4. Q 4 - Nome na vertical em escada. Modifique o programa anterior
+#           de forma a mostrar o nome em formato de escada.
 # -------------------------------------------------------------------------------------------------------------
 
 nome = str(input())
-for letra in nome:
-    print(letra)
+print(nome[0])
+for i in range(1, len(nome) + 1):
+    print(nome[0:i])
