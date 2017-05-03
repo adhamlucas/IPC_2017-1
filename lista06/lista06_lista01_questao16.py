4e0a0bafc693eea733876207e7f502caafba9ae8
KethelenTamara <kettamara@gmail.com>
2017-05-03 18:18:46 -0300

    Update lista06_lista01_questao16.py

--- lista06/lista06_lista01_questao16.py
+++ lista06/lista06_lista01_questao16.py
@@ -26,17 +26,4 @@
 #Desafio: Crie ma fórmula para chegar na posição da lista a partir do salário, sem fazer vários ifs aninhados 
 
 #---------------------------------------------------------------------------
-l=[]
-inicio=int(input("De qual valor: "))
-lim=int(input("Até qual valor: "))
-com=0
-for com in range(100000000000):
-    sal=200+(9*com)/100
-    m= ["vendedor %d" %com , sal]
-    l.append(m)
-    if sal > lim:
-        break
-    if sal >= inicio:
-        print(l[com])
-print("Número de Vendedores (ou como quiser chamar) %d" %com)
-print("Maior Salário do Povo: %d" %sal)
+
