abf2fe0d582af6ed4e5783d160c3be973f7cdd2a
tiagoweb <tiagoweb@gmail.com>
2017-05-03 16:22:10 -0400

    update

--- lista06/lista06_lista04_questao05.py
+++ lista06/lista06_lista04_questao05.py
@@ -2,6 +2,12 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
+# Adham Lucas da Silva Oliveira           1715310059
+# Gabriel Barroso da Silva Lima           1715310011
+# Guilherme Silva de Oliveira             1715310034
+# Natália Cavalcantre Xavier              1715310021
+# Tiago Ferreira Aranha	                  1715310047
+#
 # Lista 4. Q 5 - Nome na vertical em escada invertida.
 # Altere o programa anterior de modo que a escada seja invertida.
 
