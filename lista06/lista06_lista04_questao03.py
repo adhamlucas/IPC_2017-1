6f39d48addb31459390f4f954347633f5d959154
tiagoweb <tiagoweb@gmail.com>
2017-05-03 15:42:33 -0400

    update

--- lista06/lista06_lista04_questao03.py
+++ lista06/lista06_lista04_questao03.py
@@ -2,7 +2,6 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-#
 # Lista 4. Q 3 - Nome na vertical. Faça um programa que solicite o nome
 #       do usuário e imprima-o na vertical.
 # -------------------------------------------------------------------------------------------------------------
