8725f5956e8e12446a871265c22db9f9eb9a2ead
tiagoweb <tiagoweb@gmail.com>
2017-05-03 16:22:23 -0400

    update

--- lista06/lista06_lista04_questao04.py
+++ lista06/lista06_lista04_questao04.py
@@ -3,6 +3,12 @@
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
+# Adham Lucas da Silva Oliveira           1715310059
+# Gabriel Barroso da Silva Lima           1715310011
+# Guilherme Silva de Oliveira             1715310034
+# Natália Cavalcantre Xavier              1715310021
+# Tiago Ferreira Aranha	                  1715310047
+#
 # Lista 4. Q 4 - Nome na vertical em escada. Modifique o programa anterior
 #           de forma a mostrar o nome em formato de escada.
 # -------------------------------------------------------------------------------------------------------------
