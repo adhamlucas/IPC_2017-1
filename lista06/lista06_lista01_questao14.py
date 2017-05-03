623884b5f39c47fe4e0224162541c749b2c2df5b
Erik Atilio Silva Rey <erikatilio@users.noreply.github.com>
2017-05-02 20:06:10 -0400

    COLOCAR CABEÇALHO

--- lista06/lista06_lista01_questao14.py
+++ lista06/lista06_lista01_questao14.py
@@ -6,9 +6,10 @@
 # "Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
 # Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice"
 # e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
-#-------------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 
-lista = ["Telefonou para a vítima?:", "Esteve no local do crime?:", "mora perto da vítima?:", "Devia para a vítima?:", "Já trabalhou com a vítima?:"]
+lista = ["Telefonou para a vítima?:", "Esteve no local do crime?:", "mora perto da vítima?:",
+         "Devia para a vítima?:", "Já trabalhou com a vítima?:"]
 size = int(len(lista))
 sim = 0
 c = 0
@@ -22,7 +23,7 @@ if sim < 2:
     print("voce eh inocente!")
 elif sim == 2:
     print("voce eh suspeito!")
-elif 4<= sim <= 5:
+elif 4<= sim < 5:
     print("voce eh cumplice!")
 elif sim == 5:
     print("voce eh o assassino!!")
