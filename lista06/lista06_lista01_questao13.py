062f4f89f6dfd34678b664dba11cfd86d7d2e19c
andrelaborda <alln.eng@uea.edu.br>
2017-05-02 21:28:01 -0300

    GRUPO 2

--- lista06/lista06_lista01_questao13.py
+++ lista06/lista06_lista01_questao13.py
@@ -1,32 +1,44 @@
-#13)Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. Após isto,
-#calcule a média anual das temperaturas e mostre todas as temperaturas acima da média anual,
-#e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
-#-----------------------------------------------------------------------------------------------------------------------
+﻿#---------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
 
-mes = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho",
-       "agosto", "setembro", "outubro", "novembro", "dezembro"]
-temperatura_ano = []
-c = 0
+# Diego Reis Figueira      		      	1515070169
+# André Luis Laborda Neves            		1515070006
+# Gabriel Nascimento de Oliveira  	  	1715310052
+# Diogo Roberto Duarte da Costa 	  	1715310056
+# João Vitor de Cordeiro B. Gonçalves 		1515140036
 
-while c < 12:
-    print(mes[c])
-    temperatura = input("Digite a temperatura media do mês: ")
-    temperatura_ano.append(temperatura)
-    c += 1
-i = 0
-soma = 0
-while i < len(temperatura_ano):
-    soma += int(temperatura_ano[i])
-    i += 1
-media = soma / len(temperatura_ano)
-x = 0
-soma2 = 0
-print("%.2f"%media)
-while x < len(temperatura_ano):
-    if float(temperatura_ano[x]) > float(media):
-        print(x+1,"-",mes[x],"graus",end=" ")
-        print(temperatura_ano[x])
-        soma2 += 1
-    x += 1
-if soma2 == 0:
-    print("Não existe nenhum mes com temperatura acima da média")
+
+#Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma lista. Após isto, calcule a média anual
+#das temperaturas e mostre todas as temperaturas acima da média anual, e em que mês elas ocorreram
+# (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
+
+
+temperature = []
+months = ['Janeiro','Fevereiro','Março','Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+average = 0
+aboveaverage= {}
+for i in range(len(months)):
+	temperature.append(float(input('Informe a Temperatura media de ' + months[i] + ':\n')))
+	average = (average + temperature[i])
+average = average/len(months)
+
+for i in range(len(months)):
+	if(temperature[i] > average):
+ 		aboveaverage.update({months[i] : temperature[i]})
+
+print('Media das temperaturas : Anual -> ' + str(average))
+print('Meses com temperaturas acima da media: ' + str(aboveaverage))
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+# Diego Reis Figueira      		   1515070169
+# André Luis Laborda Neves           	   1515070006
+# Gabriel Nascimento de Oliveira  	   1715310052
+# Diogo Roberto Duarte da Costa 	   1715310056
+# João Vitor de Cordeiro B. Gonçalves      1515140036
---
+++ lista06/lista06_lista02_questao02.py
@@ -0,0 +1,25 @@
+﻿#---------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+
+# Diego Reis Figueira      		      	1515070169
+# André Luis Laborda Neves            		1515070006
+# Gabriel Nascimento de Oliveira  	  	1715310052
+# Diogo Roberto Duarte da Costa 	  	1715310056
+# João Vitor de Cordeiro B. Gonçalves 		1515140036
+
+
+#Escreva um algoritmo em PORTUGOL que armazene em um vetor todos os números  inteiros do intervalo fechado de 1 a 100.
+#Após isso, o algoritmo deve imprimir todos os valores armazenados.
+
+
+vetor = []
+cont = 0
+number = 1
+while (cont < 100):
+    if (number != 0):
+        vetor.append(number)
+        number = number + 1
+        cont = cont + 1
+print (vetor)
