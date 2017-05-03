3aca5b8cfdc512b424259c0fb39a48b58e54d9f5
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-03 15:36:11 -0400

    Update lista06_lista01_questao21.py

--- lista06/lista06_lista01_questao21.py
+++ lista06/lista06_lista01_questao21.py
@@ -1,3 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Erik Atilio Silva Rey                 1715310059
+# Evandro Padilha Barroso Filho         1715310009
+# Felipe Eduardo Silva de Almeida       1715310031
+# Nayara da Silva Cerdeira da Costa     1715310038
+# Joelson Pereira Lima 			        1715310060
+# Ian Gabriel Costa Machado             1215120276
+#
 """Faça um programa que carregue uma lista com os modelos de cinco carros (exemplo de modelos: FUSCA, GOL, VECTRA etc).
 Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses carros faz com um litro
 de combustível. Calcule e mostre:
@@ -33,7 +44,45 @@ Relatório Final
  3 - uno             -   12.5 -   80.0 litros - R$ 180.00
  4 - vectra          -    9.0 -  111.1 litros - R$ 250.00
  5 - peugeout        -   14.5 -   69.0 litros - R$ 155.17
+O menor consumo é do peugeout.
+Faça um programa que carregue uma lista com os modelos de cinco carros (exemplo de modelos: FUSCA, GOL, VECTRA etc).
+Carregue uma outra lista com o consumo desses carros, isto é, quantos quilômetros cada um desses carros faz com um litro
+de combustível. Calcule e mostre:
+
+    O modelo do carro mais econômico;
+    Quantos litros de combustível cada um dos carros cadastrados consome para percorrer uma distância de 1000 quilômetro
+    s
+    e quanto isto custará, considerando um que a gasolina custe R$ 2,25 o litro. Abaixo segue uma tela de exemplo. O dis
+    posição das informações deve ser o mais próxima possível ao exemplo. Os dados são fictícios e podem mudar a cada exe
+    cução do programa.
+
+Comparativo de Consumo de Combustível
+
+Veículo 1
+Nome: fusca
+Km por litro: 7
+Veículo 2
+Nome: gol
+Km por litro: 10
+Veículo 3
+Nome: uno
+Km por litro: 12.5
+Veículo 4
+Nome: Vectra
+Km por litro: 9
+Veículo 5
+Nome: Peugeout
+Km por litro: 14.5
+
+Relatório Final
+ 1 - fusca           -    7.0 -  142.9 litros - R$ 321.43
+ 2 - gol             -   10.0 -  100.0 litros - R$ 225.00
+ 3 - uno             -   12.5 -   80.0 litros - R$ 180.00
+ 4 - vectra          -    9.0 -  111.1 litros - R$ 250.00
+ 5 - peugeout        -   14.5 -   69.0 litros - R$ 155.17
 O menor consumo é do peugeout."""
+#----------------------------------------------------------------------------------------------------------------------
+
 
 cars = input().split()
 liters_km = input().split()
