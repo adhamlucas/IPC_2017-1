76c619504ff369d82cb19e12bad5c4b080fbae43
andrelaborda <alln.eng@uea.edu.br>
2017-05-02 21:32:58 -0300

    GRUPO 2

--- lista06/lista06_lista02_questão13.py
+++ lista06/lista06_lista02_questão13.py
@@ -1,13 +1,14 @@
-# ----------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-# André Luís Laborda Neves           1515070006
-# Diego Reis Figueira                1515070169
-# João Vitor De Cordeiro B Gonçalves 1515140036
-#
-#
-# 13) Escreva um algoritmo em PORTUGOL que receba dez números do usuário e
+
+# Diego Reis Figueira      		      	1515070169
+# André Luis Laborda Neves            		1515070006
+# Gabriel Nascimento de Oliveira  	  	1715310052
+# Diogo Roberto Duarte da Costa 	  	1715310056
+# João Vitor de Cordeiro B. Gonçalves 		1515140036
+#Escreva um algoritmo em PORTUGOL que receba dez números do usuário e
 #armazene em um vetor o cubo de cada número. Após isso, o algoritmo deve imprimir
 # todos os valores armazenados
 # ----------------------------------------------------------
