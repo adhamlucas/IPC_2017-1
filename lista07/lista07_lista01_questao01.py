11e18b8b220ad798beb8453c536b8213fdb3a6e3
KethelenTamara <kettamara@gmail.com>
2017-05-17 14:54:37 -0300

    Update lista07_lista01_questao01.py

--- lista07/lista07_lista01_questao01.py
+++ lista07/lista07_lista01_questao01.py
@@ -13,18 +13,4 @@
 # elementos da diagonal principal.
 #-----------------------------------------------------------------------------------------------------------------------
 
-import random
 
-linhas = 10
-colunas = 10
-matriz = []
-for g in range(linhas):
-    linha = []
-    for e in range(colunas):
-        x = random.randint(1,100)
-        linha.append(x)
-    matriz.append(linha)
-for x in range(0,10):
-    print(matriz[x])
-for e in range(0,10):
-    print(matriz[e][e])
