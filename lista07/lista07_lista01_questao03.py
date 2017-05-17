37093f97e03cf179485e57c974a8a65a0cab8539
Erik Atilio Silva Rey <erikatilio@users.noreply.github.com>
2017-05-17 12:46:23 -0400

    COLOQUEM CABEÇALHO!

--- lista07/lista07_lista01_questao03.py
+++ lista07/lista07_lista01_questao03.py
@@ -1,12 +1,21 @@
-matriz = []
-linha = []
+#3) Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva
+#somente os elementos acima da diagonal principal.
+#-----------------------------------------------------------------------------------------------------------------------
 
-for i in range(10):
-    for j in range(10):
-        linha.append(input())
+import random
+
+linhas = 10
+colunas = 10
+matriz = []
+for g in range(linhas):
+    linha = []
+    for e in range(colunas):
+        x = random.randint(1,100)
+        linha.append(x)
     matriz.append(linha)
-    
-for i in range(10):
-    for j in range(10):
-        if j > i:
-            print (matriz[i][j])
+print("Matriz:")
+for x in range(0,10):
+    print(matriz[x])
+print("Acima da diagonal principal:")
+for t in range(0,10):
+    print(matriz[t][t+1:])
