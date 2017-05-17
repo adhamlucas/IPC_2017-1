352b0dc41aafc5de9986166d113739fc6437def9
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-05-17 10:45:42 -0400

    Grupo 10

---
+++ lista07/lista07_lista01_questao09.py
@@ -0,0 +1,30 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel barroso da Silva Lima                      1715310011
+# Frederico Victor Alfaia Rodrigues                  1515200030
+# André Neves
+# Diego Figueira
+# Diogo Duarte
+#
+# Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva
+# somente os elementos acima da diagonal secundária.
+
+matriz = []
+
+for i in range (10):
+    
+    matriz.append([0]*10)
+    
+    for j in range (10):
+        
+        matriz[i][j] = int(input())
+        
+for i in range (10):
+    
+    for j in range (10):
+        
+        if i + j < 9:
+            
+            print(matriz[i][j])
