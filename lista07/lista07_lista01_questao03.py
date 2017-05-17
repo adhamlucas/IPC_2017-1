172cef5fad57df0ab3e9d43975d77eca9ec0db7f
robertacruz <ro31cruz@gmail.com>
2017-05-17 16:34:10 -0400

    Update lista07_lista01_questao03.py

--- lista07/lista07_lista01_questao03.py
+++ lista07/lista07_lista01_questao03.py
@@ -1,21 +1,30 @@
+##------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# 
+# Adham Lucas da Silva Oliveira                 1715310001
+# Jandinne Duarte de Oliveira                   1015070265
+# Roberta de Oliveira da Cruz                   0825070169
+# Nayara da Silva Cerdeira da Costa             1715310038
+# Wesley da Silva Rocha                         1715310026 
+#
+#
 #3) Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva
 #somente os elementos acima da diagonal principal.
 #-----------------------------------------------------------------------------------------------------------------------
-
-import random
-
-linhas = 10
-colunas = 10
+m = 10
+n = 10
 matriz = []
-for g in range(linhas):
-    linha = []
-    for e in range(colunas):
-        x = random.randint(1,100)
+linha = []
+
+for i in range(m):
+    for j in range(n):
+        x = int(input("Digite um valor:"))
         linha.append(x)
     matriz.append(linha)
-print("Matriz:")
-for x in range(0,10):
-    print(matriz[x])
-print("Acima da diagonal principal:")
-for t in range(0,10):
-    print(matriz[t][t+1:])
+
+for i in range(m):
+    for j in range(n):
+        if i<j:
+            print(matriz[i][j])
