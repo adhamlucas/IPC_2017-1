94bd1bac89397dd4bd93da6fe0f7782860839bb4
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-05-17 16:56:32 -0400

    Update lista07_lista01_questao26.py

--- lista07/lista07_lista01_questao26.py
+++ lista07/lista07_lista01_questao26.py
@@ -1,4 +1,15 @@
-#calcular matriz transposta
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Alexandre Marques Uchôa                   1715310028
+# Carlos Eduardo Tapudima de Oliveira	    1715310030
+# Gabriel de Queiroz Sousa                  1715310044
+# Lucas Gabriel Silveira Duarte             1715310053
+# Natália Cavalcantre Xavier                1715310021
+#
+#Criar um algoritmo que leia uma matriz ANxN (N < 10) e calcule a respectiva matriz transposta At
+
 tam = int(input('Informe tamanho da matriz quadrática: '))
 matr = [[0 for z in range(tam)] for x in range(tam)]
 
