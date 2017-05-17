51a0c0a9e0d622d2bdfc9f6a3abf16bd896ed8e6
adhamlucas <adhamlucas14@hotmail.com>
2017-05-17 16:38:20 -0400

    Update lista07_lista01_questao16.py

--- lista07/lista07_lista01_questao16.py
+++ lista07/lista07_lista01_questao16.py
@@ -1,19 +1,37 @@
+#------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# 
+# Adham Lucas da Silva Oliveira                 1715310001
+# Jandinne Duarte de Oliveira                   1015070265
+# Roberta de Oliveira da Cruz                   0825070169
+# Nayara da Silva Cerdeira da Costa             1715310038
+# Wesley da Silva Rocha                         1715310026 
+#
+#3Uma floricultura conhecedora de sua clientela gostaria de fazer um algoritmo que
+#pudesse controlar sempre um estoque mínimo de determinadas plantas, pois todo dias,
+#pela manhã, o dono faz novas aquisições. Criar um algoritmo que deixe cadastrar 50
+#tipos de plantas e nunca deixar o estoque ficar abaixo do ideal. Para cada planta, o
+#dono gostaria de cadastrar o nome, o estoque ideal e a quantidade em estoque. Dessa
+#forma o algoritmo pode calcular a quantidade que o dono da loja precisa comprar no
+#próximo dia. Essa quantidade a ser comprada deve ser impressa (quando maior que
+#zero) como uma lista para o dono da floricultura.
+#---------------------------------------------------------------------------------------
 matriz = []
-for i in range(3):
+estoque_necessario = []
+limite = 3
+
+for x in range(3):
+    things = input('Digite as informações (Nome/Est. Ideal/Qtd. Estoque: ').split()
+    matriz.append(things)
+
+for i, element in enumerate(matriz):
     linha = []
-    linha.append(raw_input("nome da planta: "))
-    linha.append(int(input("quantidade ideal: ")))
-    linha.append(int(input("quantidade em estoque: ")))
-    matriz.append(linha)
-a_comprar = []
-for i in range(3):
-    linha_b = []
-    linha_b.append(matriz[i][0])
-    linha_b.append("deve comprar: ")
-    if matriz[i][2] - matriz[i][1] > 0:
-        linha_b.append(0)
-    else:
-        linha_b.append(matriz[i][1] - matriz[i][2])
-    a_comprar.append(linha_b)
-for i in range(3):
-    print(a_comprar[i])
+    if int(element[2]) < int(element[1]):
+        necessary = int(element[1]) - int(element[2])
+        linha.append(element[0])
+        linha.append(necessary)
+        estoque_necessario.append(linha)
+
+print(estoque_necessario)
