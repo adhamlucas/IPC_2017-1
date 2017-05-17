41073e07ed89d4790d978c0a4a4e4489e2487fca
Diogo Duarte <duartediogo98@gmail.com>
2017-05-17 15:49:40 -0400

    Update lista07_lista01_questao17.py

--- lista07/lista07_lista01_questao17.py
+++ lista07/lista07_lista01_questao17.py
@@ -1,18 +1,18 @@
 #----------------------------------------------------------------------------------------------------------------------
-# Introdução a Programação de Computadores - IPC
+# IntroduÃ§Ã£o a ProgramaÃ§Ã£o de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
 # Diogo Duarte  1715310056
-# Dayana Picanço 1715310058
-# Walter Nobre 
+# Dayana PicanÃ§o 1715310058
+# Walter Nobre 1715310057
 matriz = []
 
 for i in range(5):
     linha = []
-    linha.append(raw_input("Nome da funcionária: "))
-    linha.append(int(input("quantas 'mãos' ela fêz: "))*10)
-    linha.append(int(input("quantos pés: "))*15)
-    linha.append(int(input("quantos serviços de podologia: "))*30)
+    linha.append(raw_input("Nome da funcionÃ¡ria: "))
+    linha.append(int(input("quantas 'mÃ£os' ela fÃªz: "))*10)
+    linha.append(int(input("quantos pÃ©s: "))*15)
+    linha.append(int(input("quantos serviÃ§os de podologia: "))*30)
     matriz.append(linha)
 for i in range(5):
     print("salario da %s: ") % matriz[i][0]
