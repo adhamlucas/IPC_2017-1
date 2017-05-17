cb85c75001c0e9aacd52cb51ab2eb6e8682f0fb2
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-16 22:02:59 -0400

    Update lista07_lista01_questao28.py

--- lista07/lista07_lista01_questao28.py
+++ lista07/lista07_lista01_questao28.py
@@ -23,21 +23,18 @@ for i in range(linhas):
     for j in range(colunas):
         linha.append(int(input()))
     matriz.append(linha)
-print(matriz)
 
 for i in range(colunas):
     linha = []
     for j in range(linhas):
         linha.append(int(matriz[j][i]))
     matriz_transposta.append(linha)
-print(matriz_transposta)
 
 for i in range(linhas):
     linha = []
     for j in range(colunas):
         linha.append(int(matriz[i][j]) * -1)
     matriz_oposta.append(linha)
-print(matriz_oposta)
 
 if matriz_oposta == matriz_transposta:
     print("Anti simetricas")
