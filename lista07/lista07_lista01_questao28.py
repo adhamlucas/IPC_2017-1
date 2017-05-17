51bf9a44ed8b422677c1a4b214cce0fba65a9487
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-16 22:02:09 -0400

    Update lista07_lista01_questao28.py

--- lista07/lista07_lista01_questao28.py
+++ lista07/lista07_lista01_questao28.py
@@ -1,3 +1,18 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IP
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Erik Atilio Silva Rey                 1715310059
+# Evandro Padilha Barroso Filho         1715310009
+# Felipe Eduardo Silva de Almeida       1715310031
+# Joelson Pereira Lima 			        1715310060
+# Víctor Hugo de Oliveira Carreir      1715310063
+#
+#25) Criar um algoritmo que leia valores para uma matriz M2x2. Calcular e imprimir o
+#determinante. Para cálculo do determinante de uma matriz de ordem 2, é simplesmente
+#computar a diferença entre os produtos das diagonais principal e secundária,
+#respectivamente.
+#----------------------------------------------------------------------------------------------------------------------
 linhas = int(input())
 colunas = int(input())
 matriz = []
