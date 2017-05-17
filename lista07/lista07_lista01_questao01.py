5da31393279903137fe69a9117d3dd6d57653f37
Aracille <cily.barbosa@gmail.com>
2017-05-17 13:31:45 -0400

    Update lista07_lista01_questao01.py

--- lista07/lista07_lista01_questao01.py
+++ lista07/lista07_lista01_questao01.py
@@ -1,5 +1,16 @@
-#1) Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva os
-#elementos da diagonal principal.
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# 
+# Aracille de Souza Barbosa                     1315120206
+# Kethelen Tamara Braga Barbosa                 1525212002
+# Marcus Vinicius Paes da Silva Santos          1515070060
+# Ulisses Antonio Antonino da Costa             1515090555
+# Uriel Brito Barros                            1515120558 
+#
+# 1) Criar um algoritmo que leia os elementos de uma matriz inteira 10 x 10 e escreva os
+# elementos da diagonal principal.
 #-----------------------------------------------------------------------------------------------------------------------
 
 import random
