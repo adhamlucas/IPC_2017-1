0920cdf44cf7de230969a6339603acc2b399f1a9
Gabriel Barroso <gabriel.barroso.am@gmail.com>
2017-05-17 10:46:04 -0400

    Grupo 10

---
+++ lista07/lista07_lista01_questao11.py
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel barroso da Silva Lima                      1715310011
+# Frederico Victor Alfaia Rodrigues                  1515200030
+# André Neves
+# Diego Figueira
+# Diogo Duarte
+#
+# Entrar com valores para uma matriz A3x4. Gerar e imprimir uma matriz B que é o
+# triplo da matriz A.
+
+
+matriz = []
+triplo = []
+
+for i in range (3):
+    
+    matriz.append([0]*3)
+    
+    for j in range (4):
+        
+        matriz[i][j] = int(input())
+        
+for i in range (3):
+    
+    triplo.append([0]*3)
+    
+    for j in range (4):
+        
+        triplo[i][j] = matriz[i][j] * 3
