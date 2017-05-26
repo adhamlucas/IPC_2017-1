4c430737f1aa2f9a57638c8419cead118fd72db5
tiagoweb <tiagoweb@gmail.com>
2017-05-26 10:20:59 -0400

    up

--- lista08/ipc/matriz.py
+++ lista08/ipc/matriz.py
@@ -2,12 +2,19 @@
 
 import random
 
+# função para criar matriz de ordem n x m
 def cria_matriz(n, m):
     matriz = []
     for i in range(n):
         list = []
         for j in range(m):
+            # serão inseridos valores aleatórios na matriz (entre 1 e 4)
             value = random.randint(1, 4)
             list.append(value)
         matriz.append(list)
     return matriz
+
+# função para imprimir uma matriz de qualquer ordem
+def imprime_matriz(matriz):
+    for i in matriz:
+        print(i)
