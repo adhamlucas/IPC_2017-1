6e13fe8bc661869aa90b1b6326b213cd49308ca4
tiagoweb <tiagoweb@gmail.com>
2017-05-26 10:10:55 -0400

    up

---
+++ lista08/lista08_lista01_questao01.py
@@ -0,0 +1,22 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Edson de Lima Barros   1715310043
+# Tiago Ferreira Aranha  1715310047
+# Vitor Simôes Azevedo   1715310025
+#
+# Faça um programa para imprimir:
+#    1
+#    2   2
+#    3   3   3
+#    .....
+#    n   n   n   n   n   n  ... n
+
+from lista08.ipc import vetor
+
+qtd = int(input("Informe a quantidade: "))
+
+resposta  = vetor.imprimir_questao1(qtd)
+
+print(resposta)
