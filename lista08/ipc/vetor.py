3e29ca4779601632156e88b2a5e0bdda64ebf43f
tiagoweb <tiagoweb@gmail.com>
2017-05-26 10:27:47 -0400

    up

--- lista08/ipc/vetor.py
+++ lista08/ipc/vetor.py
@@ -14,3 +14,18 @@ def imprimir_questao1(n):
         c += 1
     return res
 
+# função para retornar uma lista a partir de uma entrada em sequência
+# Ex.: Em um input: 4 10 PM, retornará a lista: ['4', '10', 'PM']
+def get_as_uri(valores):
+    valores = valores.split()
+    return valores
+
+# função para retornar uma lista de inteiros a partir de uma entrada em sequência
+# Ex.: Em um input: 4 5 6 3 2, retornará a lista: [4, 5, 6, 3, 2]
+def get_as_uri_inteiros(valores):
+    valores = valores.split()
+    valores_int = []
+    for i in valores:
+        valores_int.append(int(i))
+
+    return valores_int
