64e2e922b33801f361a66e6aedf54f09f531a1a0
tiagoweb <tiagoweb@gmail.com>
2017-05-26 10:19:33 -0400

    up

--- lista08/ipc/matriz.py
+++ lista08/ipc/matriz.py
@@ -1,3 +1,13 @@
 # Biblioteca de funções para as matrizes
 
 import random
+
+def cria_matriz(n, m):
+    matriz = []
+    for i in range(n):
+        list = []
+        for j in range(m):
+            value = random.randint(1, 4)
+            list.append(value)
+        matriz.append(list)
+    return matriz
