2987c93570666e84af339e24e7eeef4acb7a80d7
Edson de Lima Barros <edson.limajr@gmail.com>
2017-05-29 16:34:46 -0400

    up

--- lista08/lista08_lista02_questao42.py
+++ lista08/lista08_lista02_questao42.py
@@ -6,7 +6,7 @@
 # Tiago Ferreira Aranha         1715310047
 # Vitor Simôes Azevedo          1715310025
 # Roberta de Oliveira da Cruz   0825070169
-#
+# Uriel Brito Barros            1515120558
 #
 # 42. Faça uma função que receba, por parâmetro, uma matriz A(8,8)
 # e retorne o menor valor dos elementos acima da diagonal secundária.
