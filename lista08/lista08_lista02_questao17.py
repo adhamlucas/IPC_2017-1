befcd46474596da56ecc87ace84ad22cf5b48487
tiagoweb <tiagoweb@gmail.com>
2017-05-29 13:37:23 -0400

    up

---
+++ lista08/lista08_lista02_questao17.py
@@ -0,0 +1,22 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Edson de Lima Barros   1715310043
+# Tiago Ferreira Aranha  1715310047
+# Vitor Simôes Azevedo   1715310025
+#
+# 17. Faça um procedimento que recebe, por parâmetro,
+# 2 vetores de 10 elementos inteiros
+# e que calcule e retorne, também por parâmetro,
+# o vetor diferença dos dois primeiros.
+
+
+from lista08.ipc import vetor
+
+vetor1 = vetor.cria_vetor(10)
+vetor2 = vetor.cria_vetor(10)
+
+vetor_diferenca = vetor.vetor_diferenca(vetor1, vetor2)
+
+print(vetor_diferenca)
