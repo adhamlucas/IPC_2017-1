7965bc5e8c1923ba41885ba8faddd025d3255192
Edson de Lima Barros <edson.limajr@gmail.com>
2017-05-29 16:37:45 -0400

    uo

--- lista08/lista08_lista02_questao17.py
+++ lista08/lista08_lista02_questao17.py
@@ -5,6 +5,8 @@
 # Edson de Lima Barros   1715310043
 # Tiago Ferreira Aranha  1715310047
 # Vitor Simôes Azevedo   1715310025
+# Roberta de Oliveira da Cruz   0825070169
+# Uriel Brito Barros            1515120558
 #
 # 17. Faça um procedimento que recebe, por parâmetro,
 # 2 vetores de 10 elementos inteiros
