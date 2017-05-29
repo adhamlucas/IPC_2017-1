191fb69f5b8ad188a95184aceaf0caf3f03c820b
Edson de Lima Barros <edson.limajr@gmail.com>
2017-05-29 16:36:36 -0400

    up

--- lista08/lista08_lista02_questao22.py
+++ lista08/lista08_lista02_questao22.py
@@ -5,6 +5,8 @@
 # Edson de Lima Barros   1715310043
 # Tiago Ferreira Aranha  1715310047
 # Vitor Simôes Azevedo   1715310025
+# Roberta de Oliveira da Cruz   0825070169
+# Uriel Brito Barros            1515120558
 #
 # 22. Faça um procedimento que receba, por parâmetro um vetor B(50) de reais e
 # calcula o maior valor do vetor.
