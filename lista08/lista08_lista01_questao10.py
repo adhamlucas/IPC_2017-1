d91bd5de1c99d51db87c0f688b1112e3fb691a83
Joelson Lima <joelson174@users.noreply.github.com>
2017-05-30 13:39:18 -0400

    Adicionando cabeçalho

--- lista08/lista08_lista01_questao10.py
+++ lista08/lista08_lista01_questao10.py
@@ -1,3 +1,20 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Erik Atilio Silva Rey                 1715310059
+# Evandro Padilha Barroso Filho         1715310009
+# Felipe Eduardo Silva de Almeida       1715310031
+# Joelson Pereira Lima			1715310060
+# Víctor Hugo de Oliveira Carreira      1715310063
+#
+#Jogo de Craps. Faça um programa de implemente um jogo de Craps. O jogador lança um par de dados, obtendo um valor entre 2 e 12.
+#Se, na primeira jogada, você tirar 7 ou 11, você um "natural" e ganhou. Se você tirar 2, 3 ou 12 na primeira jogada, isto é
+#chamado de "craps" e você perdeu. Se, na primeira jogada, você fez um 4, 5, 6, 8, 9 ou 10,este é seu "Ponto". Seu objetivo agora é
+#continuar jogando os dados até tirar este número novamente. Você perde, no entanto, se tirar um 7 antes de tirar este Ponto
+#novamente. 
+#-----------------------------------------------------------------------------------------------------------------------
+
 from ipc import vetor
 
 inicio = input("Digite qualquer tecla para começar: ")
