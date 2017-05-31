f02d70efef08a67055f3b37ac31185876acb231d
Evandro Padilha Barroso Filho <evandro7047301@gmail.com>
2017-05-31 10:03:15 -0400

    Add files via upload

--- lista08/lista08_lista02_questao32.py
+++ lista08/lista08_lista02_questao32.py
@@ -1,27 +1,39 @@
-# ----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-# Edson de Lima Barros   1715310043
-# Tiago Ferreira Aranha  1715310047
-# Vitor Simôes Azevedo   1715310025
-# Uriel Brito Barros     1515120558
-
-# 32. Faça um procedimento que recebe, por parâmetro, uma matriz M(6,6) e um valor A .
+# Erik Atilio Silva Rey                 1715310059
+# Evandro Padilha Barroso Filho         1715310009
+# Felipe Eduardo Silva de Almeida       1715310031
+# Joelson Pereira Lima 			        1715310060
+# Víctor Hugo de Oliveira Carreira      1715310063
 #
-# O procedimento deve multiplicar cada elemento de M por A e armazenar em um vetor V(36).
-#
-# O vetor V deve retornar por parâmetro.
+#32.Faça um procedimento que recebe, por parâmetro, uma matriz M(6,6) e um valor A . O procedimento deve multiplicar cada
+# elemento de M por A e armazenar em um vetor V(36). O vetor V deve retornar por parâmetro.
+#-----------------------------------------------------------------------------------------------------------------------
+import random
+def matriz_mult(x,y):
+    A = int(input())
+    matriz = []
+    for i in range(x):
+        linha = []
+        for j in range(y):
+            linha.append(random.randint(1,1) * A)
+        matriz.append(linha)
+
+    return matriz
+
+def matriz_no_vetor(matriz):
+    vetor = []
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            vetor.append(matriz[i][j])
+    return vetor
 
 
-from lista08.ipc import matriz
 
-matriz1 = matriz.cria_matriz_quadrada(6)
 
-print(matriz1)
+print(matriz_no_vetor(matriz_mult(6,6)))
 
-A = int(input("Informe o valor que multiplicará os elementos da matriz: "))
 
-vetor = matriz.multiplica_matriz_por_inteiro(matriz1, 2)
 
-print(vetor)
