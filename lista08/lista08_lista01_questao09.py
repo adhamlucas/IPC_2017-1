3bea772d90d6aeec455665456703e03c9372b7ba
KethelenTamara <kettamara@gmail.com>
2017-05-31 15:05:10 -0300

    lista08_lista01_questao09.py

--- lista08/lista08_lista01_questao09.py
+++ lista08/lista08_lista01_questao09.py
@@ -1,3 +1,16 @@
+Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# 
+# Aracille de Souza Barbosa                     1315120206
+# Kethelen Tamara Braga Barbosa                 1525212002
+# Marcus Vinicius Paes da Silva Santos          1515070060
+# Ulisses Antonio Antonino da Costa             1515090555
+#
+
+
+
+
 #Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
 
 n=[1,2,7]
