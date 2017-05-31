fc36c48dca883074f8e6f9044703d81f2d765e30
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-05-31 16:19:34 -0400

    cabeçalho

--- lista08/lista08_lista02_questao12.py
+++ lista08/lista08_lista02_questao12.py
@@ -1,3 +1,17 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel de Queiroz Sousa                  1715310044
+# Lucas Gabriel Silveira Duarte             1715310053
+#
+#  Faça uma função que recebe, por parâmetro, a altura (alt) e o 
+#sexo de uma pessoa e retorna o seu peso ideal. 
+#Para homens, calcular o peso ideal usando 
+#a fórmula peso ideal = 72.7 x alt - 58 e, 
+#para mulheres, peso ideal = 62.1 x alt - 44.7.  
+#----------------------------------------------------------------------------------------------------------------------
+
 altura = float(input("digite a sua altura em metros: "))
 
 sexo = input("Informe seu sexo(m para masculino, f para feminino): ")
