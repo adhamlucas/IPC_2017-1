536569fc3fdd7b1f70555c12e32d8d523b59f675
Cadu (Carlos Eduardo) <cetdo@outlook.com>
2017-05-31 10:35:08 -0400

    Ajeitando o cabeçalho (de novo)
    mdssss, esse sono tá me matando

--- lista08/lista08_lista02_questao27.py
+++ lista08/lista08_lista02_questao27.py
@@ -8,8 +8,8 @@
 # Walter Nobre                              1715310057
 # Dayana Picanço Marquez                    1715310058
 #
-# 36. Faça um procedimento que receba, por parâmetro, uma matriz A(6,6) e multiplica cada linha
-# pelo elemento da diagonal principal daquela linha. O procedimento deve retornar a matriz alterada.
+# 27. Faça uma função que receba, por parâmetro, uma matriz A(6,6) e retorna o menor elemento
+# da sua dagonal secundária.
 #-----------------------------------------------------------------------------------------------------------------------
 import ipc.matriz
 
