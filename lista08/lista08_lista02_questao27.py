829fd856fd85e47b1b7f20b89915e7a930ed9d39
Cadu (Carlos Eduardo) <cetdo@outlook.com>
2017-05-31 10:31:40 -0400

    Adicionando cabeçalho

--- lista08/lista08_lista02_questao27.py
+++ lista08/lista08_lista02_questao27.py
@@ -1,3 +1,16 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Natália Cavalcantre Xavier                1715310021
+# Diogo Roberto Duarte da Costa             1715310056
+# Carlos Eduardo Tapudima de Oliveira	      1715310030
+# Walter Nobre                              1715310057
+# Dayana Picanço Marquez                    1715310058
+#
+# 36. Faça um procedimento que receba, por parâmetro, uma matriz A(6,6) e multiplica cada linha
+# pelo elemento da diagonal principal daquela linha. O procedimento deve retornar a matriz alterada.
+#-----------------------------------------------------------------------------------------------------------------------
 import ipc.matriz
 
 matrix = ipc.matriz.cria_matriz_quadrada(6)
