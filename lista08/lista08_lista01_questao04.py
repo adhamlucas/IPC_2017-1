78a861d54f0c2f5053c40ff70d271fd4574c7252
LucaSilveira <lucas.gabrielsd@gmail.com>
2017-05-31 16:13:57 -0400

    cabeçalho

--- lista08/lista08_lista01_questao04.py
+++ lista08/lista08_lista01_questao04.py
@@ -1,3 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel de Queiroz Sousa                  1715310044
+# Lucas Gabriel Silveira Duarte             1715310053
+#
+# Faça um programa, com uma função que necessite de um argumento. 
+#A função retorna o valor de caractere ‘P’, se seu argumento for positivo, 
+#e ‘N’, se seu argumento for zero ou negativo.
+#----------------------------------------------------------------------------------------------------------------------
 number = int(input("digite um número: "))
 
 def arg_porn(number):
