5c9a76d86aa76deb1f902e2b56af97174589acd7
marcuspaess <mvpdss.eng@uea.edu.br>
2017-05-31 19:42:28 -0300

    Add files via upload

--- lista08/lista08_lista02_questao19.py
+++ lista08/lista08_lista02_questao19.py
@@ -1,22 +1,46 @@
-# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------
 # Introdução a Programação de Computadores - IPC
 # Universidade do Estado do Amazonas - UEA
 # Prof. Jucimar Jr
-# Edson de Lima Barros   1715310043
-# Tiago Ferreira Aranha  1715310047
-# Vitor Simôes Azevedo   1715310025
-# Roberta de Oliveira da Cruz   0825070169
-# Uriel Brito Barros            1515120558
+# Aracille de Souza Barbosa              1315120206
+# Diego Reis Figueira                    1515070169
+# Kethelen Tamara Braga Barbosa          1525212002
+# Marcus Vinicius Paes da Silva Santos   1515070060
+# Ulisses Antonio Antonino da Costa      1515090555
 #
-# 19. Faça um procedimento que recebe, por parâmetro,
-# 2 vetores de 15 elementos inteiros e que calcule e retorne,
-# também por parâmetro, o vetor produto dos dois primeiros.
+# 19. Faça um procedimento que recebe, por parâmetro, 2 vetores de 15 elementos inteiros
+# e que calcule e retorne, também por parâmetro, o vetor produto dos dois primeiros
+# ----------------------------------------------------------
 
-from lista08.ipc import vetor
+# função que retorna o produto de dois vetores de inteiros
 
-vetor1 = vetor.cria_vetor(10)
-vetor2 = vetor.cria_vetor(10)
+def vetor_produto(v1, v2):
+    vetor_produto = []
 
-vetor = vetor.vetor_produto(vetor1, vetor2)
 
-print(vetor)
+    for i in range(0, 15):
+
+        vetor_produto.append(v1[i]*v2[i])
+    return vetor_produto
+
+
+limit = 15
+count = 0
+vetor1 = []
+
+while count < limit:
+    number = int(input("Digite um valor "))
+    vetor1.append(number)
+    count += 1
+
+count = 0
+vetor2 = []
+
+while count < limit:
+    number = int(input("Digite um valor "))
+    vetor2.append(number)
+    count += 1
+
+
+vetorproduto = vetor_produto(vetor1, vetor2)
+print(vetorproduto)
