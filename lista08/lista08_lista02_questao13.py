3e652ea3476b66bc70e1ea1911811207324ba2a6
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-05-31 23:11:24 -0400

    Update lista08_lista02_questao13.py

--- lista08/lista08_lista02_questao13.py
+++ lista08/lista08_lista02_questao13.py
@@ -1,6 +1,16 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel de Queiroz Sousa                  1715310044
+# Lucas Gabriel Silveira Duarte             1715310053
+# Ian Gabriel Costa Machado                 1215120276
+# Rodrigo Duarte de Souza                   1115140049
+# 
 #Faça um procedimento que recebe, por parâmetro,
 # um vetor A(25) de inteiros e substitui todos os valores negativos de A por zero.
 #  O vetor A deve retornar alterado.
+#----------------------------------------------------------------------------------------------------------------------
 from ipc import vetor
 
 def substituir_negativos_por_zero(vetor):
