d9f6737651048d2c2fd989b6498245e5a5eee098
Gabriel Queiroz <gabriel.dequeirozsousa@gmail.com>
2017-05-31 23:13:01 -0400

    Update lista08_lista01_questao12.py

--- lista08/lista08_lista01_questao12.py
+++ lista08/lista08_lista01_questao12.py
@@ -1,3 +1,17 @@
+#----------------------------------------------------------------------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Gabriel de Queiroz Sousa                  1715310044
+# Lucas Gabriel Silveira Duarte             1715310053
+# Ian Gabriel Costa Machado                 1215120276
+# Rodrigo Duarte de Souza                   1115140049
+#
+# Construa uma função que receba uma string como parâmetro e devolva outra string com
+# os carateres embaralhados. Por exemplo: se função receber a palavra python, pode retornar
+# npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória. Padronize em
+# sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados.  
+#----------------------------------------------------------------------------------------------------------------------
 import random
 
 def embaralhar_palavra(word):
